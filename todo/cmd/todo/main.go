c38a398032c6d55e1c31d17b7f9235d331d32e6a
Piotr Szymański <p.szymanski@bidpy.pl>
2023-02-03 19:47:37 +0100

    todo with cli args

---
+++ todo/cmd/todo/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-cmd-book/todo"
+	"os"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool.", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2020\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+	task := flag.String("task", "", "Task to be included in the Todo list")
+	list := flag.Bool("list", false, "List of all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
+	l := &todo.List{}
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case *list:
+		for _, i := range *l {
+			if !i.Done {
+				fmt.Println(i.Task)
+			}
+		}
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	case *task != "":
+		l.Add(*task)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "No flags provided")
+		os.Exit(1)
+	}
+}
---
+++ todo/cmd/todo/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const binName = "todo"
+
+// executes once per test suite
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+
+	// executes in the path of this file
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Connot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests ...")
+	result := m.Run()
+	fmt.Println("Cleaning up ...")
+	os.Remove(binName)
+	os.Remove(todoFileName)
+	os.Exit(result) // have to exit on my own according to docs
+}
+
+func TestTodoCLI(t *testing.T) {
+	task := "test task number 1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTask", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-task", task)
+		assert.Nil(t, cmd.Run())
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+
+		assert.Nil(t, err)
+		assert.Equal(t, task+"\n", string(out))
+	})
+}
