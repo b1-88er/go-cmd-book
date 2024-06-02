c9ddd8eb0f6f17d58b96fae78384d655d8564c8b
Piotr Szymański <p.szymanski@bidpy.pl>
2024-06-03 00:08:26 +0200

    apis: all server tests pass

--- apis/todoServer/server_test.go
+++ apis/todoServer/server_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"go-cmd-book/todo"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -51,6 +52,10 @@ var (
 	err  error
 )
 
+func TestMain(m *testing.M) {
+	log.SetOutput(io.Discard)
+	os.Exit(m.Run())
+}
 func TestGet(t *testing.T) {
 	url, cleanUp := setupApi(t)
 	defer cleanUp()
@@ -104,4 +109,94 @@ func TestGet(t *testing.T) {
 		assert.Equal(t, 404, r.StatusCode)
 	})
 
+}
+
+func TestAdd(t *testing.T) {
+	url, cleanup := setupApi(t)
+	defer cleanup()
+
+	taskName := "Task no. 3"
+	t.Run("Add", func(t *testing.T) {
+		body := bytes.Buffer{}
+		item := struct {
+			Task string `json:"task"`
+		}{
+			Task: taskName,
+		}
+
+		err := json.NewEncoder(&body).Encode(&item)
+		assert.NoError(t, err)
+
+		r, err := http.Post(url+"/todo", "application/json", &body)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusCreated, r.StatusCode)
+	})
+
+	t.Run("CheckIfAdded", func(t *testing.T) {
+		r, err := http.Get(url + "/todo/3")
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, r.StatusCode)
+
+		defer r.Body.Close()
+		err = json.NewDecoder(r.Body).Decode(&resp)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, resp.TotalResults)
+		assert.Equal(t, taskName, resp.Results[0].Task)
+
+	})
+}
+
+func TestDelete(t *testing.T) {
+	url, cleanup := setupApi(t)
+	defer cleanup()
+
+	t.Run("delete", func(t *testing.T) {
+		u := fmt.Sprintf("%s/todo/1", url)
+		req, err := http.NewRequest(http.MethodDelete, u, nil)
+		assert.NoError(t, err)
+		r, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, r.StatusCode)
+	})
+	t.Run("check delete", func(t *testing.T) {
+		r, err := http.Get(url + "/todo/")
+		assert.NoError(t, err)
+
+		err = json.NewDecoder(r.Body).Decode(&resp)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, resp.TotalResults)
+		assert.Equal(t, "Task number 2", resp.Results[0].Task)
+	})
+}
+
+func TestComplete(t *testing.T) {
+	url, cleanup := setupApi(t)
+	defer cleanup()
+
+	t.Run("CheckComplete", func(t *testing.T) {
+		r, err := http.Get(url + "/todo/1")
+		assert.NoError(t, err)
+		err = json.NewDecoder(r.Body).Decode(&resp)
+		assert.NoError(t, err)
+
+		assert.Equal(t, false, resp.Results[0].Done)
+	})
+
+	t.Run("Complete", func(t *testing.T) {
+		u := fmt.Sprintf("%s/todo/1?complete", url)
+		req, err := http.NewRequest(http.MethodPatch, u, nil)
+		assert.NoError(t, err)
+		r, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusNoContent, r.StatusCode)
+	})
+	t.Run("CheckComplete", func(t *testing.T) {
+		r, err := http.Get(url + "/todo/1")
+		assert.NoError(t, err)
+		err = json.NewDecoder(r.Body).Decode(&resp)
+		assert.NoError(t, err)
+
+		assert.Equal(t, true, resp.Results[0].Done)
+	})
 }
